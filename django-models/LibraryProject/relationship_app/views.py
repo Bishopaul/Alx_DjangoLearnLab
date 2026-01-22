016914b789257aa0e27e8ad78e5ad2c9b0d2aaeb
Bishopaul <monitepaul@gmail.com>
2026-01-22 17:46:11 +0100

    Finalized

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+
+def library_detail(request, library_id):
+    library = get_object_or_404(Library, id=library_id)
+    return render(request, "relationship_app/library_detail.html", {"library": library})
